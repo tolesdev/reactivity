bfde1f76fa4330378a27e9b76bb30d880d3a07f5
btoles <brennan.toles@gmail.com>
2018-11-13 20:42:28 -0600

    ci: Add versioning tool semantix and code minification

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
+    plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties'],
+    env: {
+        production: {
+            presets: ['minify']
+      }
+    }
 };
