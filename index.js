8a9f72390d20381a134452961666c5a1444d691e
Brennan <btoles@balanceinnovations.com>
2020-06-26 16:40:44 -0500

    fix: Exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./reactivity.min.js');
+    module.exports = require('./dist/reactivity.min.js');
 } else {
-    module.exports = require('./reactivity.js');
+    module.exports = require('./dist/reactivity.js');
 }
